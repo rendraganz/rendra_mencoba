44ad61ea1b6211f882d8ac6509165d3bb92c8849
rendraganz <rendrairawan170@gmail.com>
2025-03-10 18:06:14 +0700

    segi3_pascal.dart

--- segi3_pascal.dart
+++ segi3_pascal.dart
@@ -1,19 +1,18 @@
 import 'dart:io';
 
-// Fungsi rekursif untuk menghitung nilai dalam Segitiga Pascal
 int pascal(int n, int k) {
   if (k == 0 || k == n) {
-    return 1; // Base case
+    return 1; 
   }
-  return pascal(n - 1, k - 1) + pascal(n - 1, k); // Rekursi
+  return pascal(n - 1, k - 1) + pascal(n - 1, k); 
 }
 
 void main() {
   const int rows = 5;
 
-  // Cetak Segitiga Pascal dengan format rapi
+  
   for (int i = 0; i < rows; i++) {
-    stdout.write(" " * (rows - i)); // Spasi untuk merapikan tampilan
+    stdout.write(" " * (rows - i)); 
     for (int j = 0; j <= i; j++) {
       stdout.write("${pascal(i, j)} ".padLeft(2, ' '));
     }
